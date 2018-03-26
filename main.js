267ab5f4c2cf147cc90651b209e0062cedb81614
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-03-26 13:57:36 +0200

    added main.js and added code to index.html

---
+++ main.js
@@ -0,0 +1,19 @@
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+
+
+
+function preload() {
+    
+}
+
+
+
+function create() {
+    
+}
+
+
+
+function update() {
+	
+}
