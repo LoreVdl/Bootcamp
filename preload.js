d6428640aa27ae687f243268430a35825f0952f2
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-03-27 16:14:13 +0200

    error

--- preload.js
+++ preload.js
@@ -19,8 +19,7 @@ preload = {
         // characters sprites
         game.load.atlasJSONArray('characters', 'assets/characters/characters.png', 'assets/characters/characters.json');
 
-        game.load.spritesheet("levels", "assets/sprites/LevelScreen", game.global.thumbWidth, game.global.thumbHeight);
-		game.load.spritesheet("level_arrows", "assets/level_arrows.png", 48, 48);
+		    game.load.spritesheet("level_arrows", "assets/level_arrows.png", 48, 48);
 
         game.load.spritesheet("level1", "assets/sprites/Level1.png", game.global.thumbWidth, game.global.thumbHeight);
         game.load.spritesheet("level2", "assets/sprites/Level2.png", game.global.thumbWidth, game.global.thumbHeight);
