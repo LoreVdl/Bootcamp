31f3510a5cb9eda8fe1abaa0c72bad002ebce6ee
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-03-28 09:13:29 +0200

    added temporary functionality for shield link

--- level1.js
+++ level1.js
@@ -294,6 +294,20 @@ level1 = {
             this.createEnemyDeath(enemy.x, enemy.y);
             enemy.kill();
             player.body.velocity.y = -200;
+        } else if (character == 'link' && hurtFlag == false) {
+
+            if ((player.x + player.body.width * 0.5 > enemy.x) && (player.y + player.body.height * 0.5 > enemy.y) && (player.y - player.body.height * 0.5 < enemy.y) && player.scale.x == -1) {
+
+                this.createEnemyDeath(enemy.x, enemy.y);
+                enemy.kill();
+            } else if ((player.x + player.body.width < enemy.x) && (player.y + player.body.height * 0.5 > enemy.y) && (player.y - player.body.height * 0.5 < enemy.y) && player.scale.x == 1) {
+
+                this.createEnemyDeath(enemy.x, enemy.y);
+                enemy.kill();
+            } else {
+                this.hurtPlayer();
+            }
+
         } else {
             this.hurtPlayer();
         }
